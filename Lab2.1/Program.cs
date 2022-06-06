4f4a3a6b3611d6c47e21e7c4a11abffc62be2cdf
Илья Абрамёнок <i.abramenok@slata.com>
2022-06-06 21:06:53 +0800

    Добавьте файлы проекта.

---
+++ Lab2.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lab2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько ворон на ветке?:");
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        Console.WriteLine("На ветке " + a + " Ворона");
+                        break;
+                    case 2:
+                    case 3:
+                    case 4:
+                        Console.WriteLine("На ветке " + a + " Вороны");
+                        break;
+                    case 0:
+                    case 5:
+                    case 6:
+                    case 7:
+                    case 8:
+                    case 9:
+                        Console.WriteLine("На ветке " + a + " Ворон");
+                        break;
+                }
+                if (a > 9)
+                { Console.WriteLine("На ветке " + a + " Ворон"); }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Введите количесво целым числом");
+            }
+            
+        }
+    }
+}
